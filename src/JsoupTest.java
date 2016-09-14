6f951b72ee85d91b4c1cc5be7c4c36251f820900
Alberto Ceballos <alcebal2002@yahoo.es>
2016-09-14 19:03:57 +0200

    Initial Commit

--- src/JsoupTest.java
+++ src/JsoupTest.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
@@ -12,6 +14,9 @@ import org.jsoup.select.Elements;
 
 public class JsoupTest {
 	
+	private static long startTime = 0; 
+	private static long stopTime = 0;
+
 	public static void main (String[] args) throws IOException {
 
 		Validate.isTrue(args.length == 3, "usage: JsoupTest <initial_url> <search depth> <filter string>");
@@ -27,6 +32,8 @@ public class JsoupTest {
         //Elements imports;
         int totalInspected = 0;
         int totalFound = 0;
+        
+        startTime = System.currentTimeMillis(); 
         
         print("Search Depth : %s", searchDepth);
 		print("Intial URL   : %s", initialURL);
@@ -93,13 +100,28 @@ public class JsoupTest {
             }        	
 */
         }
+		
+		stopTime = System.currentTimeMillis(); 
+
+		long millis = stopTime - startTime;
+		long days = TimeUnit.MILLISECONDS.toDays(millis);
+		millis -= TimeUnit.DAYS.toMillis(days); 
+		long hours = TimeUnit.MILLISECONDS.toHours(millis);
+		millis -= TimeUnit.HOURS.toMillis(hours);
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+		millis -= TimeUnit.MINUTES.toMillis(minutes); 
+		long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
         print("\n");
         print(StringUtils.repeat("*", 15+initialURL.length()));
         print("Search Finished");
         print("Search Depth : %s", searchDepth);
 		print("Intial URL   : %s", initialURL);
 		print("Filter String: %s", filterString);
+		print("Start time   : %s",new Timestamp(startTime)); 
+		print("Stop time   : %s",new Timestamp(stopTime)); 
         print(StringUtils.repeat("*", 15+initialURL.length()));
+		print("Elapsed time: (%s - %s) ms - (%s hrs %s min %s secs)",stopTime,startTime,hours,minutes,seconds); 
 		print("Total Inspected URLs       : %d", totalInspected);
 		print("Total URLs matching filter : %d", totalFound);
         print(StringUtils.repeat("*", 15+initialURL.length()));
